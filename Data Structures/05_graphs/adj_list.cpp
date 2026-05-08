b6a459ebbf12f2b884b41c10963c53940baccdaa
Sai Hemanth Reddy <dshreddy03@gmail.com>
2026-05-08 08:15:16 +0530

    Implemented DFS & BFS in data structures

--- Data Structures/05_graphs/adj_list.cpp
+++ Data Structures/05_graphs/adj_list.cpp
@@ -1,6 +1,68 @@
 #include<iostream>
 using namespace std;
 
+void dfs_visit(unordered_map<int, vector<int>> &adj, unordered_map<int, bool> &visited, int node) {
+
+    if(visited[node]) return;
+    cout << node << "\t";
+    visited[node] = true;
+
+    for(int nb : adj[node]) {
+        dfs_visit(adj, visited, nb);
+    }
+}
+
+void dfs(unordered_map<int, vector<int>> &adj) {
+
+    unordered_map<int, bool> visited;
+
+    for(auto &p : adj) {
+        int node = p.first;
+        if(!visited[node]) {
+            dfs_visit(adj, visited, node);
+        }
+    }
+
+    cout << "\n";
+}
+
+void bfs(unordered_map<int, vector<int>> &adj, int start) {
+
+    if(adj.find(start) == adj.end()) return;
+
+    unordered_map<int, int> distance;
+    unordered_map<int, bool> visited;
+    queue<int> q;
+
+    q.push(start);
+    visited[start] = true;
+    distance[start] = 0;
+
+    while(!q.empty()) {
+
+        int l = q.size();
+
+        for(int i = 0; i < l; i++) {
+
+            int node = q.front();
+            q.pop();
+
+            for(int nb : adj[node]) {
+                if(!visited[nb]) {
+
+                    q.push(nb);
+                    visited[nb] = true;
+                    distance[nb] = distance[node] + 1;
+                }
+            }
+        }
+    }
+    for(const auto &p : distance) {
+        cout << "Distance between " << start << " and " << p.first << " is " << p.second << "\n";
+    }
+    cout << "\n";
+}
+
 int main() {
     
     unordered_map<int, vector<int>> adj_list;
@@ -16,4 +78,12 @@ int main() {
     adj_list[2] = {0, 1, 3, 4};
     adj_list[3] = {1, 2};
     adj_list[4] = {2};
+
+    cout << "DFS Traversal: ";
+    dfs(adj_list);  
+    cout<<"\n";
+    
+    cout << "BFS Traversal: \n";
+    bfs(adj_list, 0);
+    bfs(adj_list, 4);
 }
--- Data Structures/05_graphs/adj_matrix.cpp
+++ Data Structures/05_graphs/adj_matrix.cpp
@@ -1,23 +1,91 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+void dfs_visit(vector<vector<int>> &adj, vector<int> &visited, int node) {
+
+    int n = adj.size();
+    if(visited[node] || node < 0 || node >= n) return;
+    cout << node << "\t";
+    visited[node] = 1;
+
+    for(int nb = 0; nb < n; nb++) {
+        if(adj[node][nb]) {
+            dfs_visit(adj, visited, nb);
+        }
+    }
+}
+
+void dfs(vector<vector<int>> &adj) {
+
+    int n = adj.size();
+    vector<int> visited(n, 0);
+
+    for(int node = 0; node < n; node++) {
+        if(!visited[node]) {
+            dfs_visit(adj, visited, node);
+        }
+    }
+
+    cout << "\n";
+}
+
+void bfs(vector<vector<int>> &adj, int start) {
+
+    int n = adj.size();
+    if(start < 0 || start >= n) return;
+
+    vector<int> distance(n, -1);
+    vector<bool> visited(n, false);
+    queue<int> q;
+
+    q.push(start);
+    visited[start] = true;
+    distance[start] = 0;
+
+    while(!q.empty()) {
+
+        int l = q.size();
+
+        for(int i = 0; i < l; i++) {
+
+            int node = q.front();
+            q.pop();
+
+            for(int nb = 0; nb < n; nb++) {
+                if(adj[node][nb] && !visited[nb]) {
+
+                    q.push(nb);
+                    visited[nb] = true;
+                    distance[nb] = distance[node] + 1;
+                }
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) cout<<"Distance between "<<start<<" and "<<i<<" is "<<distance[i]<<"\n";
+    cout<<"\n";
+}
+
 int main() {
-    
-    // adj_matrx[i][j] = 1 implies that there is a node between nodes/vertices i & j
-    // If the graph is un directed then adj_matrx[i][j] = adj_matrx[j][i]
+
     /*
-        0 - 1
-        |   |
-        2 - 3
-        |
-        4
+        0 -- 1
+        |    |
+        2 -- 3
+        |    
+        4 
     */
-    const int nodes = 5;
-    int adj_matrx[nodes][nodes] = {
-        {0, 1, 0, 0, 0},
-        {1, 0, 1, 1, 0},
-        {0, 1, 0, 1, 1},
+    vector<vector<int>> adj = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 0, 1, 0},
+        {1, 0, 0, 1, 1},
         {0, 1, 1, 0, 0},
         {0, 0, 1, 0, 0}
     };
+
+    dfs(adj);
+    cout<<"\n";
+    bfs(adj, 0);
+    bfs(adj, 4);
 }
