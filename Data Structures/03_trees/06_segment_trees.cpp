9b21d063ffb7d7ccf5bd60c4d391759cd74b8d38
Sai Hemanth Reddy <dshreddy03@gmail.com>
2026-06-12 22:29:45 +0530

    Updated segment tree code

--- Data Structures/03_trees/06_segment_trees.cpp
+++ Data Structures/03_trees/06_segment_trees.cpp
@@ -1,77 +1,102 @@
 #include<iostream>
 using namespace std;
 
-class SegmentTreeNode {
+class SegTreeNode {
 public:
-    int lower_bound, upper_bound, sum;
-    SegmentTreeNode *left, *right;
-
-    SegmentTreeNode(int l, int r, int s) : lower_bound(l), upper_bound(r), sum(s) {}
+    int val = 0, lower = 0, upper = 0;
+    SegTreeNode *left = nullptr, *right = nullptr;
+    SegTreeNode(int x, int l, int r): val(x), lower(l), upper(r) {}
 };
 
 class SegmentTree {
 private:
-
-    SegmentTreeNode *root = nullptr;
+    SegTreeNode *root = nullptr;
     int n;
 
-    SegmentTreeNode* _build(vector<int> &v, int lower, int upper) {
-        if(lower > upper) return nullptr;
-        if(lower == upper) return new SegmentTreeNode(lower, lower, v[lower]);
-
-        int middle = (lower + upper) / 2;
-        SegmentTreeNode *newN = new SegmentTreeNode(lower, upper, 0);
-        newN->left = _build(v, lower, middle);
-        newN->right = _build(v, middle+1, upper);
-        newN->sum = newN->left->sum + newN->right->sum;
-
-        return newN;
+    int operation(SegTreeNode *l, SegTreeNode *r) {
+        int result = 0;
+        if(l) result += l->val;
+        if(r) result += r->val;
+        return result;
     }
 
-    // O(log n)
-    void _update(SegmentTreeNode *rt, int index, int value) {
-        int lower = rt->lower_bound, upper = rt->upper_bound;
-        int middle = (lower+upper)/2;
-        
-        if(lower == upper) {
-            rt->sum = value;
+    SegTreeNode * build(vector<int>& nums, int l, int r) {
+        if(l > r) return nullptr;
+        else if(l == r) return new SegTreeNode(nums[l], l, r);
+        else {
+            int middle = (l+r)/2;
+
+            SegTreeNode *newN = new SegTreeNode(0, l , r);
+            newN->left = build(nums, l, middle);
+            newN->right = build(nums, middle+1, r);
+            newN->val = operation(newN->left, newN->right);
+            return newN;
+        }
+    }
+
+    void update(SegTreeNode * curr, int index, int val) {
+        int l = curr->lower, r = curr->upper, middle = (l+r)/2;
+        if(l == r) {
+            curr->val = val;
             return;
         }
-
-        if(index >= lower && index <= middle) _update(rt->left, index, value);
-        else _update(rt->right, index, value);
-
-        rt->sum = rt->left->sum + rt->right->sum;
+        if(index <= middle) update(curr->left, index, val);
+        if(index > middle) update(curr->right, index, val);
+        curr->val = operation(curr->left, curr->right);
     }
 
-    // O(log n)
-    int _query(SegmentTreeNode *rt, int l, int r) {
-        int lower = rt->lower_bound, upper = rt->upper_bound;
-        int middle = (lower+upper)/2;
+    int query(SegTreeNode *curr, int p, int q) {
+        int l = curr->lower, r = curr->upper, middle = (l+r)/2;
+        if(l == p && r == q) return curr->val;
+        if(q <= middle) return query(curr->left, p, q);
+        if(p > middle) return query(curr->right, p, q);
+        return operation(query(curr->left, p, middle), query(curr->right, middle+1, q));
+    }
 
-        if(l == lower && r == upper) return rt->sum;
-        else if(r <= middle) return _query(rt->left, l, r);
-        else if(l > middle) return _query(rt->right, l, r);
-        else return _query(rt->left, l, middle) + _query(rt->right, middle+1, r);
+    void print() {
+        cout<<"==========================================\n";
+
+        queue<SegTreeNode *> q;        
+        if(root) q.push(root);
+
+        while(!q.empty()) {
+
+            int l = q.size();
+            for(int i = 0; i < l; i++) {
+                
+                SegTreeNode * u = q.front();
+                q.pop();
+
+                cout<<"val: "<<u->val<<" lower: "<<u->lower<<" upper: "<<u->upper;
+                if(i != l-1) cout<<"|--|";
+
+                if(u->left) q.push(u->left);
+                if(u->right) q.push(u->right);
+            }
+            cout<<"\n\n";
+        }
+        cout<<"==========================================\n";
     }
 
 public:
 
-    SegmentTree(vector<int> &v) {
-        n = v.size();
-        root = _build(v, 0, n-1);
+    SegmentTree(vector<int>& nums) {
+        n = nums.size();
+        root = build(nums, 0, n-1);
     }
-
-    void update(int index, int value) {
+    
+    // O(log(n))
+    void update(int index, int val) {
         if(index < 0 || index >= n) return;
-        _update(root, index, value);
+        update(root, index, val);
     }
-
-    int query(int l, int r) {
-        if(l < 0 || r >= n || l > r) return 0;
-        return _query(root, l, r);
+    
+    // O(log(n))
+    int sumRange(int left, int right) {
+        if(min(left, right) < 0 || max(left, right) >= n || left > right) return -1;
+        return query(root, left, right);
     }
-}
+};
 
 int main() {
 
